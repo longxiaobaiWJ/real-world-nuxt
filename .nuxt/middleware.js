37ddd131326618c379f9af2948f9447343ee56c5
LongXiaoBai <945636373@qq.com>
2020-09-02 17:05:02 +0800

    别名修改：@换成~

--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['authenticated'] = require('..\\middleware\\authenticated.js')
-middleware['authenticated'] = middleware['authenticated'].default || middleware['authenticated']
-
-middleware['notAuthenticated'] = require('..\\middleware\\notAuthenticated.js')
-middleware['notAuthenticated'] = middleware['notAuthenticated'].default || middleware['notAuthenticated']
-
-export default middleware
