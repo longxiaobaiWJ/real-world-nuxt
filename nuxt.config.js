a8a9c10b80d7956dbc96d25ab8b598d5cd209847
LongXiaoBai <wlan945636373@gmail.com>
2020-09-16 00:19:13 +0800

    测试vercel部署0.0.2

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     plugins: ['~/plugins/request', '~/plugins/format', '~/plugins/directive'],
     server: {
-        port: 3000, // default: 3000
+        port: 80, // default: 3000
         host: '0.0.0.0', // default: localhost,
     }
 }
